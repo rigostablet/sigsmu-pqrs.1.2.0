fb511c9e122daf26984c8d80e6ef61de3c5628ab
hucandev <hugo.acano@hotmail.com>
2025-02-04 21:15:25 -0500

    Ajuste para https para el envío de correos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,11 +22,6 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('*', function ($view)
         {
-
-            if (config('app.env') !== 'local') {
-                URL::forceScheme('https');
-            }
-
 			$user = request()->user();
 			$view->with('user', $user);
 
